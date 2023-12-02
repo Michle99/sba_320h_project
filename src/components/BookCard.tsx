a967c11241526f4dca861030c02f8db53fbc2c6f
Michle99 <sammiyonyx@gmail.com>
2023-12-01 21:51:32 -0500

    Updated the BookCard with new BookCardProps

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -2,21 +2,17 @@
 import React from 'react';
 import BookList from './BookList';
 import BookDetails from './BookDetails';
-import BookListInterface from '../types/BookList';
-import BookDetailsInterface from '../types/BookDetails';
+import { BookCardProps } from '../types/types'
 
-interface Book extends BookListInterface, BookDetailsInterface {}
-interface BookCardProps {
-  type: 'list' | 'details';
-  books: Book[];
-}
 
 const BookCard: React.FC<BookCardProps> = ({ type, books }) => {
-  return (
-    <div className="max-w-2xl mx-auto mt-8">
-      {type === 'list' ? <BookList books={books} /> : <BookDetails book={books[0]} />}
-    </div>
-  );
+    if (type === 'list') {
+      return <BookList books={books} />;
+    } else if (type === 'details' && books.length > 0) {
+      return <BookDetails book={books[0]} />;
+    } else {
+      return <div>No books to display</div>;
+    }
 };
 
 export default BookCard;
