92f28fe99f67d9cb2bbc89637ae23de1e75f676c
Michle99 <sammiyonyx@gmail.com>
2023-12-01 22:03:22 -0500

    Updated the SearchPage with Book types

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-// import BookList from '../components/BookList';
-import Book from '../types/BookList';
+import { Book } from '../types/types';
 import BookCard from '../components/BookCard';
 
 const SearchPage: React.FC = () => {
