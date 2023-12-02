c394d24adc3fa784cc53fcdfe7530948f232b712
Michle99 <sammiyonyx@gmail.com>
2023-12-01 20:23:00 -0500

    Updated the Book Display for the data.

--- src/components/BookDetails.tsx
+++ src/components/BookDetails.tsx
@@ -3,7 +3,12 @@ import React from 'react';
 interface Book {
   volumeInfo: {
     title: string;
-   
+    authors?: string[];
+    description?: string;
+    publishedDate?: string;
+    imageLinks?: {
+      thumbnail?: string;
+    };
   };
 }
 
@@ -13,8 +18,24 @@ interface BookDetailsProps {
 
 const BookDetails: React.FC<BookDetailsProps> = ({ book }) => {
   return (
-    <div>
-      <h2>{book.volumeInfo.title}</h2>
+    <div className="max-w-2xl mx-auto bg-white p-6 rounded-md shadow-md">
+      <h2 className="text-2xl font-semibold mb-4">{book.volumeInfo.title}</h2>
+      {book.volumeInfo.authors && (
+        <p className="text-gray-600 mb-2">By {book.volumeInfo.authors.join(', ')}</p>
+      )}
+      {book.volumeInfo.publishedDate && (
+        <p className="text-gray-600 mb-2">Published: {book.volumeInfo.publishedDate}</p>
+      )}
+      {book.volumeInfo.imageLinks && (
+        <img
+          src={book.volumeInfo.imageLinks.thumbnail}
+          alt={book.volumeInfo.title}
+          className="w-full h-40 object-cover mb-4"
+        />
+      )}
+      {book.volumeInfo.description && (
+        <p className="text-gray-800">{book.volumeInfo.description}</p>
+      )}
     </div>
   );
 };
