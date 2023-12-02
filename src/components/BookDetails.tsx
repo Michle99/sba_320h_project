2fea7fa45830e9e9129541404a014e680381a2c0
Michle99 <sammiyonyx@gmail.com>
2023-12-02 15:17:13 -0500

    Updated BookDetails & BookList Comps

--- src/components/BookDetails.tsx
+++ src/components/BookDetails.tsx
@@ -1,13 +1,12 @@
-// BookDetails.tsx
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { BookDetailsProps } from '../types/types';
 import { ArrowLeft } from 'react-feather';
-import { Link } from 'react-router-dom';
 
 const BookDetails: React.FC<BookDetailsProps> = ({ book }) => {
   return (
-    <div className="max-w-2xl mx-auto bg-white p-6 rounded-md shadow-md border border-gray-200">
-      <Link to="/" className="flex items-center text-blue-500 mb-4">
+    <div className="max-w-2xl mx-auto bg-white p-6 rounded-md shadow-md border border-gray-300">
+      <Link to="/" className="flex items-center text-blue-500 mb-4 transition-opacity opacity-75 hover:opacity-100">
         <ArrowLeft size={16} className="mr-1" />
         <span>Back to Home</span>
       </Link>
@@ -18,13 +17,13 @@ const BookDetails: React.FC<BookDetailsProps> = ({ book }) => {
       {book.volumeInfo.publishedDate && (
         <p className="text-gray-600 mb-2">Published: {book.volumeInfo.publishedDate}</p>
       )}
-    <img
-    src={`https://books.google.com/books/content?id=${book.id}&printsec=frontcover&img=1&zoom=1&source=gbs_api`}
-      alt={book.volumeInfo.title}
-      className="w-full h-40 object-cover object-center mb-4 rounded"
-    />
+      <img
+        src={`https://books.google.com/books/content?id=${book.id}&printsec=frontcover&img=1&zoom=1&source=gbs_api`}
+        alt={book.volumeInfo.title}
+        className="w-full h-40 object-scale-down object-center mb-4 rounded"
+      />
       {book.volumeInfo.description && (
-        <p className="text-gray-800">{book.volumeInfo.description}</p>
+        <p className="text-gray-800 mb-4">{book.volumeInfo.description}</p>
       )}
     </div>
   );
--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -1,4 +1,3 @@
-// BookList.tsx
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { BookListProps } from '../types/types';
@@ -11,21 +10,23 @@ const BookList: React.FC<BookListProps> = ({ books }) => {
         <Link
           to={`/book/${book.id}`}
           key={book.id}
-          className="focus:outline-none text-black"
+          className="focus:outline-none text-black transition-transform transform hover:scale-105"
         >
-          <div className="bg-white p-4 rounded-md shadow-md transition-transform transform hover:scale-105 border border-gray-200">
-            <h3 className="text-lg font-semibold mb-2">{book.volumeInfo.title}</h3>
-            {book.volumeInfo.authors && (
-              <p className="text-gray-600 mb-2">By {book.volumeInfo.authors.join(', ')}</p>
-            )}
+          <div className="bg-white rounded-lg overflow-hidden shadow-md border border-gray-300">
             <img
               src={`https://books.google.com/books/content?id=${book.id}&printsec=frontcover&img=1&zoom=1&source=gbs_api`}
               alt={book.volumeInfo.title}
-              className="w-full h-40 object-cover object-center mb-2 rounded"
+              className="w-full h-40 object-scale-down object-center mb-2 rounded-t"
             />
-            <div className="flex items-center mt-2">
-              <span className="text-blue-500">Details</span>
-              <ArrowRight className="ml-1" size={16} />
+            <div className="p-4">
+              <h3 className="text-lg font-semibold mb-2">{book.volumeInfo.title}</h3>
+              {book.volumeInfo.authors && (
+                <p className="text-gray-600 mb-2">By {book.volumeInfo.authors.join(', ')}</p>
+              )}
+              <div className="flex items-center">
+                <span className="text-blue-500">Details</span>
+                <ArrowRight className="ml-1" size={16} />
+              </div>
             </div>
           </div>
         </Link>
