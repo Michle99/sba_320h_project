0e10cf0bb8f45586f4056c6fe92ea86062fb3e74
Michle99 <sammiyonyx@gmail.com>
2023-12-01 21:53:12 -0500

    Updated BookList with BookListProps types

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -1,30 +1,30 @@
 import React from 'react';
-import BookListInterface from '../types/BookList';
-
-interface BookListProps {
-  books: BookListInterface[];
-}
+import { BookListProps } from '../types/types';
+import { Link } from 'react-router-dom';
 
 const BookList: React.FC<BookListProps> = ({ books }) => {
   return (
     <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       {books.map((book) => (
-        <div key={book.volumeInfo.title} className="bg-white p-4 rounded-md shadow-md">
-          <h3 className="text-lg font-semibold mb-2">{book.volumeInfo.title}</h3>
-          {book.volumeInfo.authors && (
-            <p className="text-gray-600 mb-2">By {book.volumeInfo.authors.join(', ')}</p>
-          )}
-          {book.volumeInfo.publishedDate && (
-            <p className="text-gray-600 mb-2">Published: {book.volumeInfo.publishedDate}</p>
-          )}
-          {book.volumeInfo.imageLinks && (
-            <img
-              src={book.volumeInfo.imageLinks.thumbnail}
-              alt={book.volumeInfo.title}
-              className="w-full h-40 object-cover mb-2"
-            />
-          )}
-        </div>
+        <Link
+          to={`/book/${book.id}`}
+          key={book.id}
+          className="focus:outline-none text-black"
+        >
+          <div className="bg-white p-4 rounded-md shadow-md transition-transform transform hover:scale-105">
+            <h3 className="text-lg font-semibold mb-2">{book.volumeInfo.title}</h3>
+            {book.volumeInfo.authors && (
+              <p className="text-gray-600 mb-2">By {book.volumeInfo.authors.join(', ')}</p>
+            )}
+            {book.volumeInfo.imageLinks && (
+              <img
+                src={book.volumeInfo.imageLinks.thumbnail || book.volumeInfo.imageLinks.smallThumbnail}
+                alt={book.volumeInfo.title}
+                className="w-full h-40 object-cover mb-2"
+              />
+            )}
+          </div>
+        </Link>
       ))}
     </div>
   );
