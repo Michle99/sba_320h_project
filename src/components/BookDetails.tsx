4f66a6557ef57a49041af7518218f63d2574f97d
Michle99 <sammiyonyx@gmail.com>
2023-12-02 15:03:35 -0500

    Updated BookDetails

--- src/components/BookDetails.tsx
+++ src/components/BookDetails.tsx
@@ -1,10 +1,16 @@
+// BookDetails.tsx
 import React from 'react';
 import { BookDetailsProps } from '../types/types';
-
+import { ArrowLeft } from 'react-feather';
+import { Link } from 'react-router-dom';
 
 const BookDetails: React.FC<BookDetailsProps> = ({ book }) => {
   return (
-    <div className="max-w-md mx-auto mt-8 bg-white p-4 rounded-md shadow-md border border-gray-300">
+    <div className="max-w-2xl mx-auto bg-white p-6 rounded-md shadow-md border border-gray-200">
+      <Link to="/" className="flex items-center text-blue-500 mb-4">
+        <ArrowLeft size={16} className="mr-1" />
+        <span>Back to Home</span>
+      </Link>
       <h2 className="text-2xl font-semibold mb-4">{book.volumeInfo.title}</h2>
       {book.volumeInfo.authors && (
         <p className="text-gray-600 mb-2">By {book.volumeInfo.authors.join(', ')}</p>
@@ -12,55 +18,14 @@ const BookDetails: React.FC<BookDetailsProps> = ({ book }) => {
       {book.volumeInfo.publishedDate && (
         <p className="text-gray-600 mb-2">Published: {book.volumeInfo.publishedDate}</p>
       )}
-      {book.volumeInfo.imageLinks && (
-        <img
-          src={`https://books.google.com/books/content?id=${book.id}&printsec=frontcover&img=1&zoom=1&source=gbs_api`}
-          alt={book.volumeInfo.title}
-          className="w-full h-40 object-cover object-center mb-4"
-          style={{ objectFit: 'cover'}}
-        />
-      )}
+    <img
+    src={`https://books.google.com/books/content?id=${book.id}&printsec=frontcover&img=1&zoom=1&source=gbs_api`}
+      alt={book.volumeInfo.title}
+      className="w-full h-40 object-cover object-center mb-4 rounded"
+    />
       {book.volumeInfo.description && (
         <p className="text-gray-800">{book.volumeInfo.description}</p>
       )}
-      <div className="mt-4">
-        <p className="text-gray-700">
-          <strong>Categories:</strong> {book.volumeInfo.categories?.join(', ')}
-        </p>
-        <p className="text-gray-700">
-          <strong>Page Count:</strong> {book.volumeInfo.pageCount || 'N/A'}
-        </p>
-        <p className="text-gray-700">
-          <strong>Print Type:</strong> {book.volumeInfo.printType || 'N/A'}
-        </p>
-        <p className="text-gray-700">
-          <strong>Language:</strong> {book.volumeInfo.language || 'N/A'}
-        </p>
-        <p className="text-gray-700">
-          <strong>ISBN:</strong>{' '}
-          {book.volumeInfo.industryIdentifiers?.map((identifier) => (
-            <span key={identifier.type}>{identifier.identifier}</span>
-          ))}
-        </p>
-        <p className="text-gray-700">
-          <strong>Preview Link:</strong>{' '}
-          <a href={book.volumeInfo.previewLink} target="_blank" rel="noopener noreferrer">
-            {book.volumeInfo.previewLink}
-          </a>
-        </p>
-        <p className="text-gray-700">
-          <strong>Info Link:</strong>{' '}
-          <a href={book.volumeInfo.infoLink} target="_blank" rel="noopener noreferrer">
-            {book.volumeInfo.infoLink}
-          </a>
-        </p>
-        <p className="text-gray-700">
-          <strong>Canonical Volume Link:</strong>{' '}
-          <a href={book.volumeInfo.canonicalVolumeLink} target="_blank" rel="noopener noreferrer">
-            {book.volumeInfo.canonicalVolumeLink}
-          </a>
-        </p>
-      </div>
     </div>
   );
 };
