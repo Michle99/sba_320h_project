a35c16f539d5cc26f6524ef4ce4f20c468ea4633
Michle99 <sammiyonyx@gmail.com>
2023-12-01 20:21:57 -0500

    Updated the Book display data

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -19,11 +19,29 @@ interface BookListProps {
 
 const BookList: React.FC<BookListProps> = ({ books }) => {
   return (
-    <ul>
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       {books.map((book) => (
-        <li key={book.id}>{book.volumeInfo.title}</li>
+        <div key={book.id} className="bg-white p-4 rounded-md shadow-md">
+          <h3 className="text-lg font-semibold mb-2">{book.volumeInfo.title}</h3>
+          {book.volumeInfo.authors && (
+            <p className="text-gray-600 mb-2">By {book.volumeInfo.authors.join(', ')}</p>
+          )}
+          {book.volumeInfo.publishedDate && (
+            <p className="text-gray-600 mb-2">Published: {book.volumeInfo.publishedDate}</p>
+          )}
+          {book.volumeInfo.imageLinks && (
+            <img
+              src={book.volumeInfo.imageLinks.thumbnail}
+              alt={book.volumeInfo.title}
+              className="w-full h-40 object-cover mb-2"
+            />
+          )}
+          {book.volumeInfo.description && (
+            <p className="text-gray-800">{book.volumeInfo.description.slice(0, 150)}...</p>
+          )}
+        </div>
       ))}
-    </ul>
+    </div>
   );
 };
 
