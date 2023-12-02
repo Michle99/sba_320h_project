fa605be2501c61e3cbee69df84d0d8d7e8ff84ef
Michle99 <sammiyonyx@gmail.com>
2023-12-02 14:57:35 -0500

    Updated BookList

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -1,6 +1,8 @@
+// BookList.tsx
 import React from 'react';
-import { BookListProps } from '../types/types';
 import { Link } from 'react-router-dom';
+import { BookListProps } from '../types/types';
+import { ArrowRight } from 'react-feather';
 
 const BookList: React.FC<BookListProps> = ({ books }) => {
   return (
@@ -11,19 +13,20 @@ const BookList: React.FC<BookListProps> = ({ books }) => {
           key={book.id}
           className="focus:outline-none text-black"
         >
-          <div className="bg-white p-4 rounded-md shadow-md transition-transform transform hover:scale-105">
+          <div className="bg-white p-4 rounded-md shadow-md transition-transform transform hover:scale-105 border border-gray-200">
             <h3 className="text-lg font-semibold mb-2">{book.volumeInfo.title}</h3>
             {book.volumeInfo.authors && (
               <p className="text-gray-600 mb-2">By {book.volumeInfo.authors.join(', ')}</p>
             )}
-            {book.volumeInfo.imageLinks && (
-              <img
-                src={`https://books.google.com/books/content?id=${book.id}&printsec=frontcover&img=1&zoom=1&source=gbs_api`}
-                alt={book.volumeInfo.title}
-                className="w-full h-40 object-fill mb-2"
-                style={{ objectFit: 'cover'}}
-              />
-            )}
+            <img
+              src={`https://books.google.com/books/content?id=${book.id}&printsec=frontcover&img=1&zoom=1&source=gbs_api`}
+              alt={book.volumeInfo.title}
+              className="w-full h-40 object-cover object-center mb-2 rounded"
+            />
+            <div className="flex items-center mt-2">
+              <span className="text-blue-500">Details</span>
+              <ArrowRight className="ml-1" size={16} />
+            </div>
           </div>
         </Link>
       ))}
