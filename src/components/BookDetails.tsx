bcf9a04c065ca8b3782499843404c2d0cd77894e
Michle99 <sammiyonyx@gmail.com>
2023-12-01 20:23:00 -0500

    Updated the Book Interface in BookDetails Comp to match BookList.

--- src/components/BookDetails.tsx
+++ src/components/BookDetails.tsx
@@ -3,7 +3,12 @@ import React from 'react';
 interface Book {
   volumeInfo: {
     title: string;
-   
+    authors?: string[];
+    description?: string;
+    publishedDate?: string;
+    imageLinks?: {
+      thumbnail?: string;
+    };
   };
 }
 
