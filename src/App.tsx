0275b33cc5ee4efcbe291ff248059b138a87dd20
Michle99 <sammiyonyx@gmail.com>
2023-12-02 15:10:06 -0500

    Updated App and Footer

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,23 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import SearchPage from './pages/SearchPage';
 import BookDetailsPage from './pages/BookDetailsPage';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 
 const App: React.FC = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/search/:searchTerm" element={<SearchPage />} />
-        <Route path="/book/:bookId" element={<BookDetailsPage />} />
-      </Routes>
+      <div className="flex flex-col min-h-screen">
+        <NavBar />
+        <div className="flex-1">
+          <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/search/:searchTerm" element={<SearchPage />} />
+          <Route path="/book/:bookId" element={<BookDetailsPage />} />
+        </Routes>
+        </div>
+        <Footer />
+      </div>
     </Router>
   );
 };
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-gray-200 p-4 mt-8">
-      <div className="container mx-auto text-center text-gray-600">
+    <footer className="bg-blue-500 p-4 mt-8">
+      <div className="container mx-auto text-center text-white ">
         <p>&copy; 2023 BookApp. All rights reserved.</p>
       </div>
     </footer>
