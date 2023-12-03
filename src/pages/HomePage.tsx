cad93b61fe968b91daf65400c9603e60f35f009e
Michle99 <sammiyonyx@gmail.com>
2023-12-02 21:15:22 -0500

    Changed the Page Title and Logo.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import SearchBar from '../components/SearchBar';
-import GoogleLogo from '../../public/Good_Fun.png';
+import GoogleLogo from '/Good_Fun.png';
 
 const HomePage: React.FC = () => {
   return (
     <div className="text-center mt-8">
       <div>
         <img  src={GoogleLogo} alt="Google Logo" className="w-85 mx-auto mb-2" />
-        <p className="text-xs ml-80">books</p>
+        {/* <p className="text-xs ml-80">books</p> */}
       </div>
       
       <SearchBar />
