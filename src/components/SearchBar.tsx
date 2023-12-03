26a72deab4be30c25a13840e48b2823873d4cd2a
Michle99 <sammiyonyx@gmail.com>
2023-12-02 19:38:32 -0500

    Updated the Search Button, Search input size::SearchBar.tsx

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Search } from 'react-feather'; 
 
 const SearchBar: React.FC = () => {
     const [searchTerm, setSearchTerm] = useState<string>('');
@@ -16,10 +17,10 @@ const SearchBar: React.FC = () => {
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
         placeholder="Search books..."
-        className="border border-blue-500 px-2 py-1 mr-2"
+        className="w-10/12 border rounded-2xl border-blue-500 px-2 py-1 mr-2 text-lg focus:outline-none focus:border-blue-700"
       />
-      <button onClick={handleSearch} className="bg-blue-500 text-white px-4 py-2 rounded-md">
-        Search
+      <button onClick={handleSearch} className="bg-blue-500 rounded-2xl text-white px-4 py-2">
+        <Search size={20} />
       </button>
     </div>
   );
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import SearchForm from '../components/SearchBar';
+import SearchBar from '../components/SearchBar';
 
 const HomePage: React.FC = () => {
   return (
     <div >
       <h1 className="text-4xl font-bold mb-4 text-center mt-2">Welcome to Google Books Search</h1>
-      <SearchForm />
+      <SearchBar />
     </div>
   );
 };
