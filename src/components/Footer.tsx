447fa60903848ebcf95f1a7db9a304d76705acf2
Michle99 <sammiyonyx@gmail.com>
2023-12-02 21:30:22 -0500

    Add GeekWorm to Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="bg-blue-500 p-4 mt-8">
       <div className="container mx-auto text-center text-white ">
-        <p>&copy; 2023 BookApp. All rights reserved.</p>
+        <p>&copy; 2023 GeekWorm. All rights reserved.</p>
       </div>
     </footer>
   );
