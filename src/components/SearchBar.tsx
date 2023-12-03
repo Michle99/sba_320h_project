c8e24d3d2e1821748a31d6013a1c77073e116c5b
Michle99 <sammiyonyx@gmail.com>
2023-12-02 23:23:46 -0500

    Changed search input focus color to `red-300`

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -17,7 +17,7 @@ const SearchBar: React.FC = () => {
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
         placeholder="Search books..."
-        className="w-10/12 border rounded-2xl border-red-900 px-2 py-1 mr-2 text-lg focus:outline-none focus:border-blue-700"
+        className="w-10/12 border rounded-2xl border-red-900 px-2 py-1 mr-2 text-lg focus:outline-none focus:border-red-300"
       />
       <button onClick={handleSearch} className="bg-red-900 rounded-2xl text-white px-4 py-2">
         <Search size={20} />
