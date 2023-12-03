63eb4cc5583a8cb4d98b790989928f191bb6b0f7
Michle99 <sammiyonyx@gmail.com>
2023-12-02 19:49:46 -0500

    Adjusting the Google Logo on HomePage...

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import SearchBar from '../components/SearchBar';
+import GoogleLogo from '../../public/googlelogo_color_272x92dp.png';
 
 const HomePage: React.FC = () => {
   return (
-    <div >
-      <h1 className="text-4xl font-bold mb-4 text-center mt-2">Welcome to Google Books Search</h1>
+    <div className="text-center mt-8">
+      <img  src={GoogleLogo} alt="Google Logo" className="w-16 mx-auto mb-2" />
+      <p className="text-xs text-center mt-1">books</p>
       <SearchBar />
     </div>
   );
