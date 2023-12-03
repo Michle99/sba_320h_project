e07e7bdbb90d45c2ac0bea71ea49132d3b2603c8
Michle99 <sammiyonyx@gmail.com>
2023-12-02 23:21:46 -0500

    Changed Nav & Footer colors

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-blue-500 p-4 mt-8">
+    <footer className="bg-red-900 p-4 mt-8">
       <div className="container mx-auto text-center text-white ">
         <p>&copy; 2023 GeekWorm. All rights reserved.</p>
       </div>
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -17,9 +17,9 @@ const SearchBar: React.FC = () => {
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
         placeholder="Search books..."
-        className="w-10/12 border rounded-2xl border-blue-500 px-2 py-1 mr-2 text-lg focus:outline-none focus:border-blue-700"
+        className="w-10/12 border rounded-2xl border-red-900 px-2 py-1 mr-2 text-lg focus:outline-none focus:border-blue-700"
       />
-      <button onClick={handleSearch} className="bg-blue-500 rounded-2xl text-white px-4 py-2">
+      <button onClick={handleSearch} className="bg-red-900 rounded-2xl text-white px-4 py-2">
         <Search size={20} />
       </button>
     </div>
