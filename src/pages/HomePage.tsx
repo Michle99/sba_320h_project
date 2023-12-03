8b4d1e65222fee98bcd8c17f66879a05603c9b58
Michle99 <sammiyonyx@gmail.com>
2023-12-02 21:28:10 -0500

    Updated `jpg` images to `svg`

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import SearchBar from '../components/SearchBar';
-import GoogleLogo from '/Good_Fun.png';
+import GeekWorm from '/Good_Fun.svg';
 
 const HomePage: React.FC = () => {
   return (
     <div className="text-center mt-8">
       <div>
-        <img  src={GoogleLogo} alt="Google Logo" className="w-85 mx-auto mb-2" />
+        <img  src={GeekWorm} alt="Google Logo" className="w-85 mx-auto mb-2" />
         {/* <p className="text-xs ml-80">books</p> */}
       </div>
       
