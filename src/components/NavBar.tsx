60d411c96061d686603927446e085109e59222ec
Michle99 <sammiyonyx@gmail.com>
2023-12-02 20:38:51 -0500

    Changed <span> text in NavBar.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -8,7 +8,7 @@ const NavBar: React.FC = () => {
       <div className="container mx-auto flex items-center justify-between">
         <Link to="/" className="text-white flex items-center">
           <BookOpen size={24} className="mr-2" />
-          <span className="font-semibold text-xl">BookApp</span>
+          <span className="font-semibold text-lg">🅶🅴🅴🅺🆆🅾🆁🅼</span>
         </Link>
       </div>
     </nav>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,7 +5,7 @@ import GoogleLogo from '../../public/googlelogo_color_272x92dp.png';
 const HomePage: React.FC = () => {
   return (
     <div className="text-center mt-8">
-      <img  src={GoogleLogo} alt="Google Logo" className="w-16 mx-auto mb-2" />
+      <img  src={GoogleLogo} alt="Google Logo" className="w-85 mx-auto mb-2" />
       <p className="text-xs text-center mt-1">books</p>
       <SearchBar />
     </div>
