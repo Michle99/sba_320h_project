57548d2f4d7a9ca5cecee8ba9ebe72d0da78ea17
Michle99 <sammiyonyx@gmail.com>
2023-12-04 13:11:12 -0500

    Updated the HomePage header

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import SearchBar from '../components/SearchBar';
-import GeekWorm from '/Good_Fun.svg';
+// import GeekWorm from '/Good_Fun.svg';
 
 const HomePage: React.FC = () => {
   return (
     <div className="text-center mt-8">
       <div>
-        <img  src={GeekWorm} alt="Google Logo" className="w-85 mx-auto mb-2" />
+        <h5 
+          className="text-center 
+            text-red-800 
+            font-mono 
+            hover:font-sans 
+            text-7xl 
+            font-semibold
+          "
+        >
+          GeekWorm
+        </h5>
+        {/* <img  src={GeekWorm} alt="Google Logo" className="w-85 mx-auto mb-2" /> */}
         {/* <p className="text-xs ml-80">books</p> */}
       </div>
       
