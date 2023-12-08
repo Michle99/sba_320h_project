7e8ff6fffce32ceb3529fa612eec7fe3f2fc00a6
Michle99 <sammiyonyx@gmail.com>
2023-12-08 14:53:03 -0500

    Book details is rendered correctly in `PreviewModal.test.tsx`

--- src/__test__/PreviewModal.test.tsx
+++ src/__test__/PreviewModal.test.tsx
@@ -2,8 +2,6 @@ import { render, screen } from '@testing-library/react';
 import { BrowserRouter as Router } from "react-router-dom";
 import PreviewModal from "../components/PreviewModal";
 
-
-
 describe('PreviewModal component', () => {
     const book = {
         id: '1',
@@ -12,6 +10,7 @@ describe('PreviewModal component', () => {
           authors: ['Author 1'],
           publishedDate: '2022-01-01',
           description: 'This is a book description.',
+          previewLink: 'https://example.com/preview',
         },
     };
 
@@ -36,4 +35,54 @@ describe('PreviewModal component', () => {
         const previewModalElement = screen.queryByTestId('preview-modal-el');
         expect(previewModalElement).toBeNull();
     })
+
+
+    test('It should render when isOpen is true', () => {
+        // Mock onClose
+        const onCloseMock = jest.fn();
+
+        // Mock isOpen
+        const isOpenMock = true;
+
+        render(
+            <Router>
+                <PreviewModal isOpen={isOpenMock} onClose={onCloseMock} book={book} />
+            </Router>
+        );
+
+        // Check if PreviewModal component is rendered
+        const previewModalElement = screen.getByTestId('preview-modal-el');
+        expect(previewModalElement).toBeInTheDocument();
+    });
+
+    test('It should render book details correctly', () => {
+         // Mock onClose
+         const onCloseMock = jest.fn();
+
+         // Mock isOpen
+         const isOpenMock = true;
+
+         render(
+            <Router>
+                <PreviewModal isOpen={isOpenMock} onClose={onCloseMock} book={book} />
+            </Router>
+        );
+
+        // Check if book details are rendered
+        expect(screen.getByText(/Book 1/i)).toBeInTheDocument(); // Using a case-insensitive regex
+        expect(screen.getByText(/By Author 1/i)).toBeInTheDocument();
+        expect(screen.getByText(/Published: 2022-01-01/i)).toBeInTheDocument();
+        expect(screen.getByAltText('Book 1')).toBeInTheDocument();
+
+        // Using a case-insensitive regex
+        const descriptionElement = screen.getByText(/This is a book description\./i);
+
+        expect(descriptionElement).toBeInTheDocument();
+
+    });
+
+
+
+
+
 });
