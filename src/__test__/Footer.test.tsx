975b873a9d54909fe12761529342640a69399cca
Michle99 <sammiyonyx@gmail.com>
2023-12-08 15:23:12 -0500

    `Footer.test.tsx` test completed and passed.

---
+++ src/__test__/Footer.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Footer from '../components/Footer';
+
+
+test('renders NavBar component', () => {
+    render(
+      <Router>
+        <Footer />
+      </Router>
+    );
+
+    // Check if footer copyright <p> is rendered
+    const footerCopyrightEl = screen.getByTestId('footer-copyright');
+    expect(footerCopyrightEl).toBeInTheDocument();
+
+    // Check if footer is rendered by role
+    const footerElement = screen.getByLabelText('footer');
+    expect(footerElement).toBeInTheDocument();
+});
